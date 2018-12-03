4c83f9432c6d37b479d9c9ccf42a6d9b4bae5952
Andy <andy_feng@hotmail.com>
2018-12-03 15:22:08 -0500

    setup

---
+++ initMode.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for value in {0..3}
+do
+	gpio mode $value out
+	gpio write $value 0
+done
---
+++ monitor.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+for value in {0..3}
+do
+	gpio mode $value out
+done
+
+gpio write 0 0
+gpio write 1 0
+gpio write 2 0
+
+first=$(./tempread)
+temp=first
+if [ $first -eq 1 ]; then
+echo "can't read the data"
+exit
+fi
+echo "first temprature is $first"
+while :
+do
+current=$(./tempread)
+echo "$current"
+let diff=$current-$temp
+if [ $diff -gt 1000 ] ; then
+gpio write 0 1
+fi
+if [ $diff -gt 2000 ] ; then
+gpio write 1 1
+fi
+if [ $diff -gt 3000 ] ; then
+gpio write 2 1
+fi
+done
